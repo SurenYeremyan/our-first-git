a6a8ca24db5966ddd1287c1d03e7afee49e8f648
SurenYeremyan <email>
2023-05-01 20:21:30 +0400

    all is done

---
+++ lab.js
@@ -0,0 +1,7 @@
+//git branch asuma te vor chyuxum enq gtnvum
+// git branch html hramanov stexcuma et hramanov chyux
+ //git checkout html hramanov asumenq gna html chyuxi mej
+ //git checkout html es hramanov asumenq failn
+
+ //git branch -D html es asumenq master chyuxn jnji
+ 
